7da7d7cd7fc5d3433d8ebeff0e08f88813ff07ee
Kildare Lauser <medicenkil@gmail.com>
2016-05-30 09:45:52 -0400

    parent_id changed to Schema Object (Only Test)

--- models/answer.js
+++ models/answer.js
@@ -9,12 +9,12 @@ var Question = mongoose.model('Question');
 
 var answerSchema = new Schema({
     created_at   : { type: Date, default: Date.now },
-    content      : { type: String },
-    author_id    : { type: Schema.ObjectId, ref: "User" },
-    question     : { type: Schema.ObjectId, ref: "Question" },
+    content      : { type: String, require: true },
+    author_id    : { type: Schema.ObjectId, ref: "User", require: true },
+    question     : { type: Schema.ObjectId, ref: "Question", require: true },
     n_stars      : { type: Number, default: 0 },
     deep         : { type: Number, default: 0 },
-    parent_id    : { type: Number }
+    parent_id    : { type: Schema.ObjectId, ref: "Answer" },
 });
 
 module.exports = mongoose.model('Answer', answerSchema);
