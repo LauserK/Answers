575352a5f1ccbffb54eb56c648757d52a4ae5884
Kildare Lauser <medicenkil@gmail.com>
2016-05-30 22:16:53 -0400

    Add createQuestion controller

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var slugify = require('../utils/slugify');
 
 /* Import Models */
 require('../models/user');
@@ -16,6 +17,33 @@ exports.getAllQuestions = function(req, res) {
         // If exists and error return error 500
         if (err) return res.status(500).send(err.message); 
         //Return JSON data
-        res.status(200).jsonp(questions);
+        return res.status(200).jsonp(questions);
     });           
 };
+
+/* POST /api/questions/ - Create a question */
+exports.createQuestion = function(req, res) {
+    
+    if(req.body.title == "" || req.body.title < 10 ){
+        return res
+            .status(400)
+            .jsonp({ message: {"title": "Title can't be empty"}})
+    }
+    
+    if(req.body.content == "") {
+        req.body.content = null;
+    }
+    
+    
+    var question = new Question({
+       title     : req.body.title,
+       content   : req.body.content,
+       author_id : req.user,
+       slug      : slugify.slugify(req.body.title)
+    });
+    
+    question.save(function(err, question) {
+        if(err) return res.status(500).send(err.message);
+        res.status(201).jsonp(question); 
+    });
+}
