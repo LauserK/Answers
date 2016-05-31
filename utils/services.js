505cafe7a74d2ef11ba3bfb0ed04025b37118018
Kildare Lauser <medicenkil@gmail.com>
2016-05-30 21:18:36 -0400

    Add username to jwt

--- utils/services.js
+++ utils/services.js
@@ -3,10 +3,11 @@ var moment = require('moment');
 
 exports.createToken = function(user) {  
   var payload = {
-    sub: user._id,
-    iat: moment().unix(),
-    exp: moment().add(14, "days").unix(),
-    admin: user.admin
+    sub        : user._id,
+    iat        : moment().unix(),
+    exp        : moment().add(14, "days").unix(),
+    admin      : user.admin,
+    username   : user.username
   };
 
   var token = jwt.encode(payload, 'tokenultrasecreto');
